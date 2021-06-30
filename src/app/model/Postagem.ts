6d878851a5045fdf58997014d53957dcebbf0da6
Ricardoamvp <ricardo.amvp@gmail.com>
2021-06-30 15:24:47 -0300

    Consumo da API

---
+++ src/app/model/Postagem.ts
@@ -0,0 +1,9 @@
+import { Usuario } from "./Usuario"
+
+export class Postage{
+    public id: number
+    public titulo: string
+    public texto: string
+    public data: Date
+    public usuario: Usuario
+}
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +10,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = new Usuario
+  confirmarSenha: string
+  tipoUser: string
 
-  ngOnInit(): void {
+  constructor(private authService: AuthService, private router: Router) { }
+
+  ngOnInit() {
+    window.scroll(0,0)
+  }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value
+  }
+
+  tipoUsuario(event: any) {
+    this.tipoUser = event.target.value 
+  }
+
+  cadastrar(event: any) {
+    this.usuario.tipo = this.tipoUser
+
+    if(this.usuario.senha != this.confirmarSenha) {
+      alert("As senhas estão diferentes.")
+    } else {
+        this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+          this.usuario = resp
+          this.router.navigate(["/login"])
+          alert("Usuario cadastrado com sucesso! ")
+        })
+    }
+
   }
 
 }
